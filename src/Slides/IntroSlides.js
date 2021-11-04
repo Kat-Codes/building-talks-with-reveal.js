e8f919dd2e3659570bb618c01673abf82d1c004f
Katie Walker <katiew812@hotmail.co.uk>
2021-11-04 17:03:21 +0000

    Add link to qr slide

--- src/Slides/IntroSlides.js
+++ src/Slides/IntroSlides.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Slide from '../Components/RevealComponents/Slide';
 import Note from '../Components/Notes/note';
+import Link from '../Components/Link';
 
 const IntroSlides = () => (
   <>
@@ -11,6 +12,9 @@ const IntroSlides = () => (
         alt="QR code that links to these slides"
       />
       <p color="black">✨Slides✨</p>
+      <Link href="building-talks-with-reveal-js.katcodes.co.uk/">
+        building-talks-with-reveal-js.katcodes.co.uk/
+      </Link>
       <Note>
         The slides are accessible via this QR code to follow along with. I'll
         link again at the end if you want to reference them later
