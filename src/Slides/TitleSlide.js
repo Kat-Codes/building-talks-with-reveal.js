e6b4266f020479fc4a6f84c7131847caadcd0c64
Katie Walker <katiew812@hotmail.co.uk>
2021-11-04 17:20:22 +0000

    Add twitter

--- src/Slides/TitleSlide.js
+++ src/Slides/TitleSlide.js
@@ -4,7 +4,7 @@ import Slide from '../Components/RevealComponents/Slide';
 const TitleSlide = () => (
   <Slide>
     <h2>Building talks with Reveal.js (with React)</h2>
-    <h5>Katie Walker</h5>
+    <h5>Katie Walker @KatCodes</h5>
   </Slide>
 );
 
